9a26d6004976628fcbbe80352bf4faffed00da39
kjasng <manhnguyenls131@gmail.com>
2024-12-01 02:09:15 +0700

    chore: config router for webapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,50 @@
-import { useState } from "react"
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router'
+import LoginPage from '@components/pages/login/index.tsx'
+import Header from '@components/Header/index.tsx'
+import Footer from '@components/Footer/index.tsx'
+import ContactPage from '@components/pages/contact/index.tsx'
+import BookPage from '@components/pages/book/index.tsx'
+import Home from '@components/Home/index.tsx'
 
-import { useAppDispatch, useAppSelector } from "./redux/hooks"
-import styles from "./redux/features/counter/Counter.module.css"
-import {
-  decrement,
-  increment,
-  incrementAsync,
-  incrementByAmount,
-  incrementIfOdd,
-  selectCount,
-  selectStatus,
-} from "./redux/features/counter/counterSlice"
 
-export default function Counter() {
-  const dispatch = useAppDispatch()
-  const status = useAppSelector(selectStatus)
-  const count = useAppSelector(selectCount)
-  const [incrementAmount, setIncrementAmount] = useState("2")
-
-  const incrementValue = Number(incrementAmount) || 0
-
+const Layout = () => {
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span aria-label="Count" className={styles.value}>
-          {count}
-        </span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          type="number"
-          onChange={e => {
-            setIncrementAmount(e.target.value)
-          }}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          disabled={status !== "idle"}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => {
-            dispatch(incrementIfOdd(incrementValue))
-          }}
-        >
-          Add If Odd
-        </button>
-      </div>
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
+  )
+}
+
+
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      errorElement: <div>Not Found</div>,
+
+      children: [
+        {index: true, element: <Home />},
+        {
+          path: 'contact',
+          element: <ContactPage />,
+        },
+        {
+          path: 'book',
+          element: <BookPage />,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <LoginPage />,
+    },
+  ])
+  return (
+    <>
+      <RouterProvider router={router} />
+      </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { store } from './redux/store.ts'
 import { Provider } from 'react-redux'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
